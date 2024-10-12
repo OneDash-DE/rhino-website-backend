97d6ac9e6d2506e131de8db97e9e0bc9ca998c66
D074015 <jonathan.busshoff@sap.com>
2024-10-12 17:40:29 +0200

    feat: update strapi + add canceled to events

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,25 @@
 export default ({ env }) => ({
-  "import-export-entries": {
-    enabled: process.env.NODE_ENV === "development",
-  },
   upload: {
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY_ID"),
-        secretAccessKey: env("AWS_ACCESS_SECRET"),
-        region: env("AWS_REGION"),
-        params: {
-          Bucket: env("AWS_BUCKET"),
+        s3Options: {
+          credentials: {
+            accessKeyId: env("AWS_ACCESS_KEY_ID"),
+            secretAccessKey: env("AWS_ACCESS_SECRET"),
+          },
+          region: env("AWS_REGION"),
+          params: {
+            ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
         },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
       },
     },
   },
